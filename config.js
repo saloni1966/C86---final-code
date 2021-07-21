ac86945d4072dad3bacc5669b0660adc2163d82e
Saloni Sharma <68701930+saloni1966@users.noreply.github.com>
2021-07-21 16:20:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDorjb3ChBeapuXxN6y7PjT70pMLadzA7I",
+  authDomain: "studious-sign-320506.firebaseapp.com",
+  databaseURL: "https://studious-sign-320506-default-rtdb.firebaseio.com",
+  projectId: "studious-sign-320506",
+  storageBucket: "studious-sign-320506.appspot.com",
+  messagingSenderId: "161245892507",
+  appId: "1:161245892507:web:d7634c46228303b3a21b3e"
+};
